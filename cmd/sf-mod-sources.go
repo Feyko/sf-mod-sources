2e31d2344431345dec6e29db2b7bec377a3da485
Feyko <feykook@gmail.com>
2022-10-02 01:04:43 +0200

    feat: turn into sfmodsdownload

--- cmd/sf-mod-sources.go
+++ cmd/sf-mod-sources.go
@@ -2,59 +2,86 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"git.sr.ht/~emersion/gqlclient"
+	"github.com/pkg/errors"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"sf-mod-sources/gql"
 )
 
-type ModSource struct {
-	Name string
-	URL  string
+type ModVersions struct {
+	Name     string
+	Versions []ModVersion
+}
+
+type ModVersion struct {
+	Version string
+	URL     string
 }
 
 var c int
 
 func main() {
-	if len(os.Args) < 2 {
-		log.Fatal("This program takes a path as an argument")
-	}
+	//if len(os.Args) < 2 {
+	//	log.Fatal("This program takes a path as an argument")
+	//}
 
-	out := os.Args[1]
+	//out := os.Args[1]
 
 	client := gqlclient.New("https://api.ficsit.app/v2/query", http.DefaultClient)
 
-	var r []ModSource
+	var r []ModVersions
 
 	var offset int
 
 	fmt.Print("Getting page 1... ")
 	mods := getMods(client, offset)
-	count := appendModSources(&r, mods)
+	count := appendModVersions(&r, mods)
 	fmt.Printf("Found %v mods of which %v have a source URL\n", len(mods), count)
 
 	for len(mods) == 100 {
 		offset += 100
-		fmt.Printf("Getting page %v... ", offset+1)
+		fmt.Printf("Getting page %v... ", offset/100+1)
 		mods = getMods(client, offset)
-		count := appendModSources(&r, mods)
-		fmt.Printf("Found %v mods of which %v have a source URL\n", len(mods), count)
+		appendModVersions(&r, mods)
+		fmt.Printf("Found %v mods\n", len(mods))
 	}
 
 	fmt.Println("Done searching mods!")
 
-	data, err := json.MarshalIndent(r, "", "  ")
+	wd, err := os.Getwd()
 	if err != nil {
-		log.Fatalf("Could not marshal the data into JSON: %v", err)
+		log.Fatalf("Could not get the working directory: %v", err)
 	}
 
-	err = os.WriteFile(out, data, 0644)
-	if err != nil {
-		log.Fatalf("Could not write the data to file: %v", err)
+	//errChan := make(chan error)
+	//doneChan := make(chan struct{})
+	//pool := workerpool.New(runtime.NumCPU() * 4)
+	for _, mod := range r {
+		for _, version := range mod.Versions {
+			versionDir := filepath.Join(wd, mod.Name, version.Version)
+
+			err := os.MkdirAll(versionDir, 0777)
+			if err != nil && !os.IsExist(err) {
+				log.Fatalf("Error making a mod's folder: %v", err)
+			}
+			err = downloadLinkTo("https://api.ficsit.app"+version.URL, filepath.Join(versionDir, version.Version+".zip"))
+			if err != nil {
+				log.Fatalf("Error downloading a mod: %v", err)
+			}
+		}
 	}
+
+	//go func() {
+	//	pool.StopWait()
+	//	doneChan <- struct{}{}
+	//}()
+
+	fmt.Println("Done!")
 }
 
 func getMods(client *gqlclient.Client, offset int) []gql.Mod {
@@ -69,18 +96,17 @@ func checkErr(err error) {
 	}
 }
 
-func appendModSources(sources *[]ModSource, mods []gql.Mod) int {
-	newSources := modsToModSourceSlice(mods)
+func appendModVersions(sources *[]ModVersions, mods []gql.Mod) int {
+	newSources := modsToModVersionsSlice(mods)
 	*sources = append(*sources, newSources...)
 	return len(newSources)
 }
 
-func modsToModSourceSlice(mods []gql.Mod) []ModSource {
-	r := make([]ModSource, 0, len(mods)/2)
-	c += len(mods)
+func modsToModVersionsSlice(mods []gql.Mod) []ModVersions {
+	r := make([]ModVersions, 0, len(mods)/2)
 
 	for _, mod := range mods {
-		source := modToModSource(mod)
+		source := modToModVersions(mod)
 		if source != nil {
 			r = append(r, *source)
 		}
@@ -88,12 +114,48 @@ func modsToModSourceSlice(mods []gql.Mod) []ModSource {
 	return r
 }
 
-func modToModSource(mod gql.Mod) *ModSource {
-	if mod.Source_url == nil || *mod.Source_url == "" {
+func modToModVersions(mod gql.Mod) *ModVersions {
+	if len(mod.Versions) == 0 {
 		return nil
 	}
-	return &ModSource{
-		Name: mod.Name,
-		URL:  *mod.Source_url,
+	return &ModVersions{
+		Name:     mod.Name,
+		Versions: versionsToModVersionSlice(mod.Versions),
 	}
+}
+
+func versionsToModVersionSlice(versions []gql.Version) []ModVersion {
+	//r := make([]ModVersion, len(versions))
+	//for i, version := range versions {
+	//	r[i] = versionToModVersion(version)
+	//}
+	//return r
+	return []ModVersion{versionToModVersion(versions[0])}
+}
+
+func versionToModVersion(version gql.Version) ModVersion {
+	return ModVersion{
+		Version: version.Version,
+		URL:     version.Link,
+	}
+}
+
+func downloadLinkTo(link, to string) error {
+	resp, err := http.Get(link)
+	if err != nil {
+		return errors.Wrap(err, "could not downloads the file")
+	}
+	defer resp.Body.Close()
+
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return errors.Wrap(err, "could not downloads the file")
+	}
+
+	err = os.WriteFile(to, b, 0644)
+	if err != nil {
+		return errors.Wrap(err, "could not write the file")
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -7,5 +7,8 @@ require git.sr.ht/~emersion/gqlclient v0.0.0-20220713194109-e4b2ae1fac10
 require (
 	github.com/agnivade/levenshtein v1.0.1 // indirect
 	github.com/dave/jennifer v1.4.1 // indirect
+	github.com/gammazero/deque v0.2.0 // indirect
+	github.com/gammazero/workerpool v1.1.3 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/vektah/gqlparser/v2 v2.2.0 // indirect
 )
